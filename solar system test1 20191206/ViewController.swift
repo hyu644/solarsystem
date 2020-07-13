202cbaef3c80e2864e1ea5a639bc3b6c87ebb699
hyu644 <67137241+hyu644@users.noreply.github.com>
2020-07-13 20:19:47 +0900

    first comment

---
+++ solar system test1 20191206/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  solar system test1 20191206
+//
+//  Created by hyu on 12/6/1 R.
+//  Copyright © 1 Reiwa hyu. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
---
+++ solar system test1 20191206Tests/solar_system_test1_20191206Tests.swift
@@ -0,0 +1,34 @@
+//
+//  solar_system_test1_20191206Tests.swift
+//  solar system test1 20191206Tests
+//
+//  Created by hyu on 12/6/1 R.
+//  Copyright © 1 Reiwa hyu. All rights reserved.
+//
+
+import XCTest
+@testable import solar_system_test1_20191206
+
+class solar_system_test1_20191206Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
